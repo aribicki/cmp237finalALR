4ea3d43ed2efff1ec26ad02a79d711e057a9a56e
Andrew Ribicki <aribicki@saintjoe.edu>
2015-05-06 10:45:17 -0500

    myarray for array.js

---
+++ myarray.js
@@ -0,0 +1,10 @@
+var MYARRAY_FILE = "[\n " + [
+'{"name": A", "randomnumber":"1"}',
+'{"name": B", "randomnumber":"2"}',
+'{"name": C", "randomnumber":"3"}',
+'{"name": D", "randomnumber":"4"}',
+'{"name": E", "randomnumber":"5"}',
+'{"name": F", "randomnumber":"6"}',
+'{"name": G", "randomnumber":"7"}',
+'{"name": H", "randomnumber":"8"}',
+].join(",\n") + "\n]";
