3a32325bc2d6fb3972389c6d483bd6e43e0d2437
Andrew Ribicki <aribicki@saintjoe.edu>
2015-05-06 10:46:12 -0500

    Update array.js

--- array.js
+++ array.js
@@ -1,1 +1,17 @@
-
+load('myarraynumbers.js'); 
+var myarray = JSON.parse(MYARRAY_File);       
+print(JSON.stringify(myarray);
+function filter(array, test) {
+  var passed = []
+  for (var i = 0; i < array.length; i++) {           //standard filter code
+    if (test(array[i])) {                           
+     passed.push(array[i]);
+     }
+     }
+     }
+     return passed;
+     }
+     
+      print(JSON.stringify(filter(myarray, function(number) {
+       return number.randomnumber < 45;                             
+     })));
