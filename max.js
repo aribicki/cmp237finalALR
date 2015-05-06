21ffaa130b1f6d56a8c0559509f73c7068c42c22
Andrew Ribicki <aribicki@saintjoe.edu>
2015-05-06 11:53:47 -0500

    added some more comments

--- max.js
+++ max.js
@@ -9,7 +9,7 @@ function reduce(myarray, combine, start) {
   }
 
 print(myarray.reduce(function(highest, cur){
-  highest = 1;
-  if (myarray.randomnumber > highest) return cur;
-  else return high;
+  highest = 1;      // setting current highest to 1 for base 
+  if (myarray.randomnumber > highest) return cur; // if the random number on myarray is bigger it returns that
+  else return highest;        // else it returns the current highest 
 }))
