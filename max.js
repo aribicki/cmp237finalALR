46cca4bb5b77e90798328aed9a9463d50b097eba
Andrew Ribicki <aribicki@saintjoe.edu>
2015-05-06 10:57:26 -0500

    saving

--- max.js
+++ max.js
@@ -1,1 +1,11 @@
+load('myarray.js'); 
+var myarray = JSON.parse(MYARRAY_File);
 
+function reduce(myarray, combine, start) {
+  var current = start;
+  for (var i = o; i < roster.length; i++)
+    current = combin(current, myarray[i]);
+    return current;
+  }
+
+print(myarray.reduce(function(highest, cur)))
